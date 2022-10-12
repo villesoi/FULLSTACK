214aafb5a0b580b585bd61db8a07e3a371abaab1
villesoi <soikkeli987@gmail.com>
2022-10-12 12:45:16 +0300

    01-osion tehtävät

--- 01A/Kurssitiedot/src/App.js
+++ 01A/Kurssitiedot/src/App.js
@@ -3,15 +3,15 @@ const App = () => {
     name: 'Half Stack application development',
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: 'Fundamentals of React:',
         exercises: 10
       },
       {
-        name: 'Using props to pass data',
+        name: 'Using props to pass data:',
         exercises: 7
       },
       {
-        name: 'State of a component',
+        name: 'State of a component:',
         exercises: 14
       }
     ]
@@ -23,6 +23,7 @@ const App = () => {
       <p>{course.parts[0].name} {course.parts[0].exercises} </p>
       <p>{course.parts[1].name} {course.parts[1].exercises} </p>
       <p>{course.parts[2].name} {course.parts[2].exercises} </p>
+      <p> Total of exercises: {course.parts[2].exercises+course.parts[1].exercises+course.parts[0].exercises} </p>
 
     </div>
   )
